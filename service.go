c3806a9ad841b211381b94620e625b26855b0ff9
zhangyi <zh1@vip.qq.com>
2024-01-02 14:15:23 +0800

    1

--- service.go
+++ service.go
@@ -9,36 +9,36 @@ import (
 	"time"
 )
 
-type ServiceConfig[T any] struct {
+type GRPCServiceConfig[T any] struct {
 	AppName          string
 	CreateClientFunc func(grpc.ClientConnInterface) T
 	EtcdConfig       *Config
 	EtcdInstance     *Client
 }
-type ServiceOption[T any] func(*ServiceConfig[T])
+type GRPCServiceOption[T any] func(*GRPCServiceConfig[T])
 
-func ServiceWithAppName[T any](val string) ServiceOption[T] {
-	return func(conf *ServiceConfig[T]) {
+func GRPCServiceWithAppName[T any](val string) GRPCServiceOption[T] {
+	return func(conf *GRPCServiceConfig[T]) {
 		conf.AppName = val
 	}
 }
-func ServiceWithCreateClientFunc[T any](val func(grpc.ClientConnInterface) T) ServiceOption[T] {
-	return func(conf *ServiceConfig[T]) {
+func GRPCServiceWithCreateClientFunc[T any](val func(grpc.ClientConnInterface) T) GRPCServiceOption[T] {
+	return func(conf *GRPCServiceConfig[T]) {
 		conf.CreateClientFunc = val
 	}
 }
-func ServiceWithEtcdConfig[T any](val *Config) ServiceOption[T] {
-	return func(conf *ServiceConfig[T]) {
+func GRPCServiceWithEtcdConfig[T any](val *Config) GRPCServiceOption[T] {
+	return func(conf *GRPCServiceConfig[T]) {
 		conf.EtcdConfig = val
 	}
 }
-func ServiceWithEtcdInstance[T any](val *Client) ServiceOption[T] {
-	return func(conf *ServiceConfig[T]) {
+func GRPCServiceWithEtcdInstance[T any](val *Client) GRPCServiceOption[T] {
+	return func(conf *GRPCServiceConfig[T]) {
 		conf.EtcdInstance = val
 	}
 }
 
-func NewService[T any](config *ServiceConfig[T], opts ...ServiceOption[T]) *Service[T] {
+func NewGRPCService[T any](config *GRPCServiceConfig[T], opts ...GRPCServiceOption[T]) *GRPCService[T] {
 	for _, apply := range opts {
 		if apply != nil {
 			apply(config)
@@ -57,7 +57,7 @@ func NewService[T any](config *ServiceConfig[T], opts ...ServiceOption[T]) *Serv
 		config.EtcdInstance = New(config.EtcdConfig)
 	}
 
-	s := &Service[T]{
+	s := &GRPCService[T]{
 		Discovery: MustNewDiscovery(&DiscoveryConfig{
 			AppName:      config.AppName,
 			EtcdInstance: config.EtcdInstance,
@@ -71,7 +71,7 @@ func NewService[T any](config *ServiceConfig[T], opts ...ServiceOption[T]) *Serv
 				if ok {
 					val.(*Node[T]).Conn.Close()
 				}
-				node, err := NewNodeClient[T](addr, config.CreateClientFunc)
+				node, err := NewGRPCNode[T](addr, config.CreateClientFunc)
 				if err == nil {
 					s.NodeList.Store(nodeName, node)
 				}
@@ -89,7 +89,7 @@ func NewService[T any](config *ServiceConfig[T], opts ...ServiceOption[T]) *Serv
 	return s
 }
 
-func NewNodeClient[T any](addr string, createClientFunc func(grpc.ClientConnInterface) T) (client *Node[T], err error) {
+func NewGRPCNode[T any](addr string, createClientFunc func(grpc.ClientConnInterface) T) (client *Node[T], err error) {
 	conn, err := grpc.Dial(addr,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithKeepaliveParams(keepalive.ClientParameters{
@@ -115,12 +115,12 @@ type Node[T any] struct {
 	Client T
 }
 
-type Service[T any] struct {
+type GRPCService[T any] struct {
 	*Discovery
 	NodeList *sync.Map
 }
 
-func (s *Service[T]) FindNode(nodeName string) (*Node[T], error) {
+func (s *GRPCService[T]) FindNode(nodeName string) (*Node[T], error) {
 	var node *Node[T]
 	s.NodeList.Range(func(key, value interface{}) bool {
 		if key == nodeName {
@@ -135,7 +135,7 @@ func (s *Service[T]) FindNode(nodeName string) (*Node[T], error) {
 	return node, nil
 }
 
-func (s *Service[T]) GetNodeSet() map[string]*Node[T] {
+func (s *GRPCService[T]) GetNodeSet() map[string]*Node[T] {
 	var m = make(map[string]*Node[T])
 	s.NodeList.Range(func(k, v interface{}) bool {
 		m[k.(string)] = v.(*Node[T])
